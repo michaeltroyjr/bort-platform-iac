164a7991961675b63f963c364d7ebf1f53e32b39
Mike <michaeltroyjr@gmail.com>
2026-02-02 11:53:25 -0500

    Refactor CloudFront distribution resource to improve clarity and organization

--- modules/cloudfront/main.tf
+++ modules/cloudfront/main.tf
@@ -1,28 +1,34 @@
-resource "aws_cloudfront_distribution" "s3_distribution" {
+resource "aws_cloudfront_distribution" "supreme_marines" {
+  enabled             = true
+  default_root_object = "index.html"
+
+  aliases = [
+    "supreme-marines.bortplatforms.com"
+  ]
+
   origin {
     domain_name = var.s3_bucket_domain_name
     origin_id   = "S3Origin"
   }
 
-  enabled = true
-  default_root_object = "index.html"
-
   default_cache_behavior {
-    cache_policy_id  = "658327ea-f89d-4fab-a63d-7e88639e58f6"
-    compress         = true
-    allowed_methods  = ["GET", "HEAD"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "S3Origin"
+    target_origin_id       = "S3Origin"
     viewer_protocol_policy = "redirect-to-https"
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    cache_policy_id        = "658327ea-f89d-4fab-a63d-7e88639e58f6"
+    compress               = true
+  }
+
+  viewer_certificate {
+    acm_certificate_arn      = aws_acm_certificate.cf_cert.arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2021"
   }
 
   restrictions {
     geo_restriction {
       restriction_type = "none"
     }
-  }
-
-  viewer_certificate {
-    cloudfront_default_certificate = true
   }
 }
